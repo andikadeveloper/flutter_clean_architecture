af5159d5ea8304d3a779bbc826fac773668c0145
andikadeveloper <andikarastafara93@gmail.com>
2023-02-21 08:37:03 +0700

    feat: add locator for data module

--- lib/di/locator.dart
+++ lib/di/locator.dart
@@ -1,4 +1,8 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_clean_architecture/data/datasources/user_remote_data_source.dart';
+import 'package:flutter_clean_architecture/data/repositories/user_repository_impl.dart';
+import 'package:flutter_clean_architecture/domain/repositories/user_repository.dart';
+import 'package:flutter_clean_architecture/domain/usecases/get_users.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 
@@ -18,4 +22,19 @@ void setup() {
 
     return dio;
   });
+
+  // Usecase
+  locator.registerLazySingleton<GetUsers>(
+    () => GetUsers(userRepository: locator.get()),
+  );
+
+  // Repository
+  locator.registerLazySingleton<UserRepository>(
+    () => UserRepositoryImpl(userRemoteDataSource: locator.get()),
+  );
+
+  // Data source
+  locator.registerLazySingleton<UserRemoteDataSource>(
+    () => UserRemoteDataSourceImpl(dio: locator.get()),
+  );
 }
